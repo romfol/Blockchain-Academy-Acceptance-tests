bf3e0de0203eda5c962dfadeb7a6fcab888fefe3
romfol <romanfoltyn1@gmail.com>
2018-10-25 15:17:18 +0300

    in progress

--- 1.js
+++ 1.js
@@ -1,3 +1,20 @@
 //з десяткової в шістнадцятирічну
-var num = 12;
-console.log(num.toString(16));
+const decToHex = string => {
+  string = parseInt(string);
+  return string.toString(16);
+};
+
+console.log(decToHex('12'));
+
+//з десяткової в двійкову
+const decToBin = string => {
+  string = parseInt(string);
+  return string.toString(2);
+};
+
+console.log(decToBin('7'));
+
+//з шістнадцятирічної в двійкову
+const hexToBin = string => {};
+
+console.log(hexToBin(7));
